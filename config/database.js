8e7ffacbefefdeb0be36da1dedbef9f4d4d22e21
ahmanemmanuel@gmail.com <ahmanemmanuel@gmail.com>
2018-12-22 10:51:38 +0100

    install error

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-	'secret': process.env.SECRET_KEY || /* istanbul ignore next: tired of writing tests */ 'crevance-v0.0.1',
-	'database':  process.env.MONGO_CONNECTION||'mongodb://localhost/crevance', /* istanbul ignore next: tired of writing tests */ 
+	'secret': process.env.SECRET_KEY || /* istanbul ignore next: tired of writing tests */ 'paperdaz-v0.0.1',
+	'database':  process.env.MONGO_CONNECTION||'mongodb://admin:powerfully-made22@ds157614.mlab.com:57614/paperdaz', /* istanbul ignore next: tired of writing tests */ 
 	// 'mongodb://skywalker_kfg:moyosore1991@ds125841.mlab.com:25841/crevance?connectTimeoutMS=300000'
 	// process.env.MONGO_CONNECTION ||
 };
